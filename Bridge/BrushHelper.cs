1013d7efd34e2ee89ff3ddaf335d225e8cc9d908
Hummel-Wang <SDwangfu@126.com>
2018-12-27 17:04:04 +0800

    add bridge

---
+++ Bridge/BrushHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Color
+    {
+        public string color;
+    }
+
+    class Red : Color
+    {
+        public Red()
+        {
+            this.color = "red";
+        }
+    }
+
+    class Green : Color
+    {
+        public Green()
+        {
+            this.color = "green";
+        }
+    }
+
+    class Blue : Color
+    {
+        public Blue()
+        {
+            this.color = "blue";
+        }
+    }
+
+    abstract class BrushHelper
+    {
+        protected Color c;
+        public abstract void Paint();
+        public void SetColor(Color c)
+        {
+            { this.c = c; }
+        }
+    }
+
+    class BigBrush : BrushHelper
+    {
+        public override void Paint()
+        {
+            Console.WriteLine("Using big brush and color {0} painting", c.color);
+        }
+    }
+
+    class SmallBrush : BrushHelper
+    {
+        public override void Paint()
+        {
+            Console.WriteLine("Using small brush and color {0} painting", c.color);
+        }
+    }
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BrushHelper b = new BigBrush();
+            b.SetColor(new Red());
+            b.Paint();
+            b.SetColor(new Green());
+            b.Paint();
+            b.SetColor(new Blue());
+            b.Paint();
+
+            b = new SmallBrush();
+            b.SetColor(new Red());
+            b.Paint();
+            b.SetColor(new Green());
+            b.Paint();
+            b.SetColor(new Blue());
+            b.Paint();
+
+            Console.ReadLine();
+        }
+    }
+}
