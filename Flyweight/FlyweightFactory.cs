388c917137745ae8254039ca63add973692fc20a
Hummel-Wang <SDwangfu@126.com>
2019-01-14 15:07:31 +0800

    add Flyweight

---
+++ Flyweight/FlyweightFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    /// <summary>
+    /// 享元工厂，负责创建和管理享元对象
+    /// </summary>
+    public class FlyweightFactory
+    {
+        // 最好使用泛型Dictionary<string,Flyweighy>
+        //public Dictionary<string, Flyweight> flyweights = new Dictionary<string, Flyweight>();
+        public Hashtable flyweights = new Hashtable();
+
+        public FlyweightFactory()
+        {
+            flyweights.Add("A", new ConcreteFlyweight("A"));
+            flyweights.Add("B", new ConcreteFlyweight("B"));
+            flyweights.Add("C", new ConcreteFlyweight("C"));
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            // 更好的实现如下
+            //Flyweight flyweight = flyweights[key] as Flyweight;
+            //if (flyweight == null)
+            //{
+            // Console.WriteLine("驻留池中不存在字符串" + key);
+            // flyweight = new ConcreteFlyweight(key);
+            //}
+            //return flyweight;
+
+            return flyweights[key] as Flyweight;
+        }
+    }
+
+    /// <summary>
+    ///  抽象享元类，提供具体享元类具有的方法
+    /// </summary>
+    public abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+
+    // 具体的享元对象，这样我们不把每个字母设计成一个单独的类了，而是作为把共享的字母作为享元对象的内部状态
+    public class ConcreteFlyweight : Flyweight
+    {
+        // 内部状态
+        private string intrinsicstate;
+
+        // 构造函数
+        public ConcreteFlyweight(string innerState)
+        {
+            this.intrinsicstate = innerState;
+        }
+
+        /// <summary>
+        /// 享元类的实例方法
+        /// </summary>
+        /// <param name="extrinsicstate">外部状态</param>
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("具体实现类: intrinsicstate {0}, extrinsicstate {1}", intrinsicstate, extrinsicstate);
+        }
+    }
+}
