15beb584d0b78ae35639aa920527d2e068dabc1a
Hummel-Wang <SDwangfu@126.com>
2019-01-14 15:57:34 +0800

    add stragety

---
+++ Stragety/ITaxStragety.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stragety
+{
+    // 所得税计算策略
+    public interface ITaxStragety
+    {
+        double CalculateTax(double income);
+    }
+
+    // 个人所得税
+    public class PersonalTaxStrategy : ITaxStragety
+    {
+        public double CalculateTax(double income)
+        {
+            return income * 0.12;
+        }
+    }
+
+    // 企业所得税
+    public class EnterpriseTaxStrategy : ITaxStragety
+    {
+        public double CalculateTax(double income)
+        {
+            return (income - 3500) > 0 ? (income - 3500) * 0.045 : 0.0;
+        }
+    }
+
+    public class InterestOperation
+    {
+        private ITaxStragety m_strategy;
+        public InterestOperation(ITaxStragety strategy)
+        {
+            this.m_strategy = strategy;
+        }
+
+        public double GetTax(double income)
+        {
+            return m_strategy.CalculateTax(income);
+        }
+    }
+}
