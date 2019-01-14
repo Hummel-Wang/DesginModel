1c8b4f77bc9dbfc0cefb06e29f79d51635626e30
Hummel-Wang <SDwangfu@126.com>
2019-01-14 14:53:51 +0800

    -madd Facade

---
+++ Facade/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class Customer
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public Customer(string name)
+        {
+            this._name = name;
+        }
+    }
+
+    /// <summary>
+    /// 银行子系统
+    /// </summary>
+    public class Bank
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 信用子系统
+    /// </summary>
+    public class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 贷款子系统
+    /// </summary>
+    public class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Program
+    {
+        private const int _amount = 12000;
+        static void Main(string[] args)
+        {
+            #region 正常模式
+            //Bank bank = new Bank();
+            //Loan loan = new Loan();
+            //Credit credit = new Credit();
+
+            //Customer customer = new Customer("Ann McKinsey");
+
+            //bool eligible = true;
+            //if (!bank.HasSufficientSavings(customer, _amount))
+            //{
+            //    eligible = false;
+            //}
+            //else if (!loan.HasNoBadLoans(customer))
+            //{
+            //    eligible = false;
+            //}
+            //else if (!credit.HasGoodCredit(customer))
+            //{
+            //    eligible = false;
+            //}
+
+            //Console.WriteLine("\n" + customer.Name + " has been " + (eligible ? "Approved" : "Rejected"));
+            //Console.ReadLine();
+            #endregion
+
+            #region 外观者模式
+            Mortgage mortgage = new Mortgage();
+            Customer customer = new Customer("Ann McKinsey");
+            bool eligable = mortgage.IsEligible(customer, 125000);
+            Console.WriteLine("\n" + customer.Name + " has been " + (eligable ? "Approved" : "Rejected"));
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
