6ce00e3c374d2ed9a1bf1b1f1ea4b3a37571d186
Hummel-Wang <SDwangfu@126.com>
2019-01-14 16:09:03 +0800

    add vistor and memento

---
+++ Vistor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vistor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectStructure objectStructure = new ObjectStructure();
+            foreach (Element e in objectStructure.Elements)
+            {
+                // 每个元素接受访问者访问
+                e.Accept(new ConcreteVistor());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
