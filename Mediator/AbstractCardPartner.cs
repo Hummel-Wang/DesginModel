f2dca4c835ac0aa9ee9f3e4f635ac259134656a5
Hummel-Wang <SDwangfu@126.com>
2019-01-14 15:49:24 +0800

    add Mediator

---
+++ Mediator/AbstractCardPartner.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    // 抽象牌友类
+    public abstract class AbstractCardPartner
+    {
+        public int MoneyCount { get; set; }
+        public AbstractCardPartner()
+        {
+            MoneyCount = 0;
+        }
+        public abstract void ChangeCount(int Count, AbstractMediator mediator);
+    }
+
+    /// <summary>
+    /// 牌友A
+    /// </summary>
+    public class ParterA : AbstractCardPartner
+    {
+        // 依赖与抽象中介者对象
+        public override void ChangeCount(int Count, AbstractMediator mediator)
+        {
+            mediator.AWin(Count);
+        }
+    }
+    /// <summary>
+    /// 牌友B
+    /// </summary>
+    public class ParterB : AbstractCardPartner
+    {
+        // 依赖与抽象中介者对象
+        public override void ChangeCount(int Count, AbstractMediator mediator)
+        {
+            mediator.BWin(Count);
+        }
+    }
+    /// <summary>
+    /// 抽象中介者类
+    /// </summary>
+    public abstract class AbstractMediator
+    {
+        protected AbstractCardPartner A;
+        protected AbstractCardPartner B;
+             public AbstractMediator(AbstractCardPartner a, AbstractCardPartner b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public abstract void AWin(int count);
+        public abstract void BWin(int count);
+    }
+
+    /// <summary>
+    /// 具体中介者类
+    /// </summary>
+    public class MediatorPater : AbstractMediator
+    {
+        public MediatorPater(AbstractCardPartner a, AbstractCardPartner b) : base(a, b)
+        {
+
+        }
+        public override void AWin(int count)
+        {
+            A.MoneyCount += count;
+            B.MoneyCount -= count;
+        }
+        public override void BWin(int count)
+        {
+            B.MoneyCount += count;
+            A.MoneyCount -= count;
+        }
+    }
+
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractCardPartner A = new ParterA();
+            AbstractCardPartner B = new ParterB();
+            // 初始钱
+            A.MoneyCount = 20;
+            B.MoneyCount = 20;
+            AbstractMediator mediator = new MediatorPater(A, B);
+
+            //A赢钱了
+            A.ChangeCount(5, mediator);
+            Console.WriteLine("A 现在的钱是：{0}", A.MoneyCount);// 应该是25
+            Console.WriteLine("B 现在的钱是：{0}", B.MoneyCount); // 应该是15
+            //B赢钱了
+            B.ChangeCount(10, mediator);
+            Console.WriteLine("A 现在的钱是：{0}", A.MoneyCount);// 应该是15
+            Console.WriteLine("B 现在的钱是：{0}", B.MoneyCount); // 应该是25
+
+            Console.ReadLine();
+        }
+    }
+}
